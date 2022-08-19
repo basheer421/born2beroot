c2feb533d7ed1edc2e7ff5b6bb1e9e38f6afbe03
basheer421 <109145360+basheer421@users.noreply.github.com>
2022-08-19 22:44:56 +0400

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,6 +3,6 @@
 echo -n "#Architecture: ";	uname -a;
 echo -n "#CPU physical : ";	lscpu | awk 'NR==5{print $NF}';
 echo -n "#vCPU : ";		cat /proc/cpuinfo | grep processor | wc -l;
-echo -n "#Memory Usage: ";	free --mega | grep Me | awk '{
-printf("%s/%sMB (%4.2f%%)\n", $3, $2, 100*($3/$2))
-}'
+echo -n "#Memory Usage: ";	free --mega | grep Me | awk '{printf("%s/%sMB (%.2f%%)\n", $3, $2, 100*($3/$2))}';
+echo -n "#Disk Usage: ";	df --total -m -x tmpfs | awk 'END{
+printf("%s/%.0fGb (%.0f%%)\n", $3, $2/1000, 100*$3/$2)}';
